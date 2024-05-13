c915efbd478c85c36a0c21e386459f93fe49aae2
SJ <127341132+minari314159@users.noreply.github.com>
2024-05-13 09:51:16 -0400

    Config backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,23 +3,22 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
-	baseURL: import.meta.env.VITE_API_URL,
-
+	baseURL: import.meta.env.BACKEND_URL,
 });
 
 // axios is automatically handling tokens for jwt by adding the auth headers
 api.interceptors.request.use(
 	(config) => {
-		const token = localStorage.getItem(ACCESS_TOKEN)
+		const token = localStorage.getItem(ACCESS_TOKEN);
 		if (token) {
-			config.headers.Authorization = `Bearer ${token}`
+			config.headers.Authorization = `Bearer ${token}`;
 		}
-		return config
+		return config;
 	},
 	(error) => {
-		return Promise.reject(error)
+		return Promise.reject(error);
 	}
-)
+);
 
 //use api object instead of axios directly so authorization is always added directly
 export default api;
