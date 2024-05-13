3d81dc084a85754da1c5ca3a5190bf35f3d724b5
SJ <127341132+minari314159@users.noreply.github.com>
2024-05-13 13:21:00 -0400

    chore: Update API base URL to production server

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
-	baseURL: import.meta.env.VITE_API_URL,
+	baseURL: "https://nanocellumstore-production.up.railway.app/",
 });
 
 // axios is automatically handling tokens for jwt by adding the auth headers
