e959247df720a351d91c46b8d69049f25a4df5fc
SJ <127341132+minari314159@users.noreply.github.com>
2024-05-13 11:30:41 -0400

    chore: Update Procfile and Dockerfile for backend server configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -18,7 +18,7 @@ import os
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'djoser',
     'api',
-    'core'
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -177,7 +177,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join('BASE_DIR','static'),)
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
