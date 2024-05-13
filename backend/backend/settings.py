634bd5a27479b0be25050a630ef77bd0b9bb0f00
SJ <127341132+minari314159@users.noreply.github.com>
2024-05-13 10:36:47 -0400

    Fix style sheet loading

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -177,7 +177,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ['/static']
+STATICFILES_DIRS = ['BASE_DIR','/static']
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
