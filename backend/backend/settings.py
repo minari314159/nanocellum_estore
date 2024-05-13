aa64974800a29279e91c4db4776a1b362636c3eb
SJ <127341132+minari314159@users.noreply.github.com>
2024-05-13 09:58:18 -0400

    chore: Update STATICFILES_DIRS to fix static file paths

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -177,7 +177,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = '/static'
+STATICFILES_DIRS = ['/static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
