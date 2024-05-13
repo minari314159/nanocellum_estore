ead8e1b6378caaa2c54ab66e90819105cc7199bc
SJ <127341132+minari314159@users.noreply.github.com>
2024-05-13 13:59:31 -0400

    chore: Update STATIC_ROOT to fix static file paths

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
