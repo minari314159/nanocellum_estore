3dab7511220e4e2adabd45a706bb5cf39dcd6c54
SJ <127341132+minari314159@users.noreply.github.com>
2024-07-26 16:34:30 -0400

    commit

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,10 @@ app.use(cors(corsOptions));
 
 //cors headers
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+	res.header("Access-Control-Allow-Origin", [
+		"http://localhost:5173",
+		"https://nanocellum-store.vercel.app",
+	]);
 	res.header("Access-Control-Allow-Headers", true);
 	res.header("Access-Control-Allow-Credentials", true);
 	res.header(
