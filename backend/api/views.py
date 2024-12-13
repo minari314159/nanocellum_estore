37861ad6444d4d36dc680b884c103fc561a9ab02
SJ <127341132+minari314159@users.noreply.github.com>
2024-12-13 15:20:18 -0500

    finish cart endpoint viewset

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
-from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import CreateAPIView
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import CartSerializer, ReviewSerializer, UserSerializer, ProductSerializer, CartItemSerializer
+from .serializers import CartSerializer, ReviewSerializer, UserSerializer, ProductSerializer, CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer
 from .models import Cart, CartItem, Product, Review
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny, IsAdminUser
 
@@ -37,32 +37,9 @@ class ProductViewSet(ModelViewSet):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    lookup_field = 'id'
-
-    def get_permissions(self):
-        """
-        Set permissions dynamically:
-        - Allow anyone for GET requests.
-        - Require admin privileges for POST, PUT, DELETE.
-        """
-        if self.request.method == 'GET':
-            return [AllowAny()]
-        return [IsAdminUser()]
-
-    def delete(self, request, id):
-        """Delete a product (admin only)."""
-        product = get_object_or_404(Product, pk=id)
-        product.delete()
-        return Response(
-            {"detail": "Product deleted successfully."},
-            status=status.HTTP_204_NO_CONTENT)
-
-
 class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         return Review.objects.filter(product_id=self.kwargs['product_pk'])
@@ -92,11 +69,9 @@ class CartItemViewSet(ModelViewSet):
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
-            pass
-            # return AddCartItemSerializer
+            return AddCartItemSerializer
         elif self.request.method == 'PATCH':
-            pass
-            # return UpdateCartItemSerializer
+            return UpdateCartItemSerializer
         return CartItemSerializer
 
     def get_serializer_context(self):
